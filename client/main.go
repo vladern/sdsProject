654dd35c182d18f2008c7801806e798c9cf37de0
vladernn <vladys.3234@gmail.com>
2018-03-27 17:52:01 +0200

    login con el servidor este último devolviendo el token

--- client/main.go
+++ client/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// función para comprobar errores (ahorra escritura)
+func chk(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	Client()
+	client()
+}
+
+/***
+CLIENTE
+***/
+
+// gestiona el modo cliente
+func client() {
+
+	/* creamos un cliente especial que no comprueba la validez de los certificados
+	esto es necesario por que usamos certificados autofirmados (para pruebas) */
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	// # Login
+	var user string
+	var password string
+	fmt.Print("Introduce tu usuario: ")
+	fmt.Scanf("%s\n", &user)
+	fmt.Print("Introduce tu contraseña: ")
+	fmt.Scanf("%s\n", &password)
+
+	// ** ejemplo de registro
+	data := url.Values{}           // estructura para contener los valores
+	data.Set("user", user)         // comando (string)
+	data.Set("password", password) // usuario (string)
+
+	r, err := client.PostForm("https://localhost:10443/login", data) // enviamos por POST
+	chk(err)
+	fmt.Println("Respuesta:")
+	io.Copy(os.Stdout, r.Body) // mostramos el cuerpo de la respuesta (es un reader)
+	fmt.Println()
 }
